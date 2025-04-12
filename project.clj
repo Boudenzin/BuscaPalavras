fcec19b29cfa5d44877d9c38fdebc4f6a386fa7b
romildo <romildo-rrj@hotmail.com>
2025-04-11 21:49:47 -0300

    feat: iniciar projeto com estrutura base usando Leiningen

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject word-search "0.1.0-SNAPSHOT"
+  :description "Projeto simples de caça-palavras em Clojure"
+  :url "http://exemplo.com/word-search"
+  :license {:name "MIT"
+            :url "https://opensource.org/licenses/MIT"}
+  :dependencies [[org.clojure/clojure "1.11.1"]]
+  :main word-search.core
+  :repl-options {:init-ns word-search.core})
---
+++ src/cacapalavras/core.clj
@@ -0,0 +1,6 @@
+(ns word-search.core)
+
+(defn -main []
+  (println "Bem-vindo ao caça-palavras em Clojure!")
+  
+  )
