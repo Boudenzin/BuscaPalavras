8338c77376c180f33adb9aa19b9d8fbd4d1203f9
Romildo Júnior <139826286+Boudenzin@users.noreply.github.com>
2025-04-15 09:08:05 -0300

    fix: Atualiza para Clojure CLI

--- project.clj
+++
@@ -1,8 +0,0 @@
-(defproject word-search "0.1.0-SNAPSHOT"
-  :description "Projeto simples de caça-palavras em Clojure"
-  :url "http://exemplo.com/word-search"
-  :license {:name "MIT"
-            :url "https://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/clojure "1.11.1"]]
-  :main caca-palavras.core
-  :repl-options {:init-ns caca-palavras.core})
