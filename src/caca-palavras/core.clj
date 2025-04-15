1a84ab157dd408ae43c3a060e3f5ff7eaabdee04
Romildo Júnior <139826286+Boudenzin@users.noreply.github.com>
2025-04-15 08:45:32 -0300

    feat: Adiciona buffer de .txt

--- src/caca-palavras/core.clj
+++ src/caca-palavras/core.clj
@@ -1,4 +1,15 @@
-(ns caca-palavras.core)
+(ns caca-palavras.core
+  (:require [clojure.java.io :as io]))
+
+(defn ler-matriz [caminho]
+  (with-open [rdr (io/reader caminho)]
+    (doall
+     (map (fn [linha]
+            (vec (remove #(Character/isWhitespace %) linha)))
+          (line-seq rdr)))))
+;; => [[\C \A \Ç \A]
+;;     [\P \A \L \A]
+;;     [\V \R \A \S]]
 
 (defn -main []
   (println "Bem-vindo ao caça-palavras em Clojure!")
